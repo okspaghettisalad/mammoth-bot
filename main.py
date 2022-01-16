28845009438564a385581186d7ce70165c2ae165
okspaghettisalad <polarpanda987@gmail.com>
2022-01-16 15:34:23 -0500

    !incarnations formatting

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ async def incarnations(ctx, cmd=None, arg=None):
     if cmd == None:
         await ctx.send('\
 **Commands:**\n\
-> > !mammoth <type> | talking mammoth\n\
-> > !doomsday | doomsday\n\
-> > !noises | play noises\n\
-> > !extinction | disconnect bot from channel\n\n\
-> > !origins | bot info\n\
-!incarnations <command> for more info*', reference=ctx.message)
+> > **!mammoth <type>** | *talking mammoth*\n\
+> > **!doomsday** | *doomsday*\n\
+> > **!noises** | *play noises*\n\
+> > **!extinction** | *disconnect mammoth from channel*\n\
+> > **!origins** | *bot information*\n\
+*!incarnations <command> for more info*', reference=ctx.message)
 
 
     # each line gets its own send() because otherwise every embed would appear below all messages
@@ -27,16 +27,18 @@ async def incarnations(ctx, cmd=None, arg=None):
         if arg == None:
             await ctx.send('\
 **!mammoth <type> | mammoth image based on <type>**\n\
-> > mammoth bot will reply to the message the command replyed to, or will reply to the last message otherwise\n\
-> > *bot will also delete the command message\n\
+> mammoth bot will reply to the message the command replyed to, or will reply to the last message otherwise\n\
+> (*bot will delete the command message*)\n\
+\n\
 **Types:**\n\
-> > no type provided: the classic mammoth.\n\
-> > now: NOW! (youtyu  be ,com /watch?v=T-BOPr7NXME)\n\
-> > pe / desolate: it\'s an empty husk.\n\
-> > ghastly: blink and chance never catching another glimpse\n\
-> > ansenstor: they know where to throw a good party\n\
-*!incarnations mammoth <type> for more info*\n\
-> *(<type> = classic for defaul mammoth)*', reference=ctx.message)
+> > **no type provided** | *the classic mammoth.*\n\
+> > **now** | *NOW! (youtyu  be ,com /watch?v=T-BOPr7NXME)*\n\
+> > **pe / desolate** | *it\'s an empty husk.*\n\
+> > **ghastly** | *blink and chance never catching another glimpse*\n\
+> > **ansestor** | *they know where to throw a good party*\n\
+\n\
+!incarnations mammoth <type> *for more info*\n\
+> *<type> =* classic *for info about defualt mammoth*', reference=ctx.message)
 
 
         elif str(arg).lower() == 'classic': await ctx.send('\
@@ -49,7 +51,7 @@ https://cdn.discordapp.com/attachments/851897450385768503/931657627547611176/CRE
 
         elif str(arg).lower() == 'pe' or str(arg).lower() == 'desolate': await ctx.send('\
 https://cdn.discordapp.com/attachments/818925860379557928/932031686932320326/IMG_0403.png\n\
-> **type:  pe  |  type:  desolate**', reference=ctx.message)
+> **type:  pe / desolate**', reference=ctx.message)
 
         elif str(arg).lower() == 'ghastly': await ctx.send('\
 https://cdn.discordapp.com/attachments/818925860379557928/932032179029028894/unknown.png\n\
@@ -76,7 +78,7 @@ async def mammoth(ctx, arg=None):
     elif str(arg).lower() == 'ghastly':
         link = 'https://cdn.discordapp.com/attachments/818925860379557928/932032179029028894/unknown.png'
 
-    elif str(arg).lower == 'ancestor':
+    elif str(arg).lower() == 'ancestor':
         link = 'https://cdn.discordapp.com/attachments/818925860379557928/932034707410014278/IMG_0407.png'
 
     else: link = 'https://media.discordapp.net/attachments/802674631717814312/915674691283333130/m.gif'
@@ -129,8 +131,8 @@ async def extinction(ctx):
 @bot.command()
 async def origins(ctx):
     await ctx.send('\
-https://github.com/okspaghettisalad/mammoth-bot\n\
-*bot by okspaghettisalad#0056*')
+> *bot by okspaghettisalad#0056*\n\
+> https://github.com/okspaghettisalad/mammoth-bot', reference=ctx.message)
 
 
 """@client.event
